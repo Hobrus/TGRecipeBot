c32f252cfedd466fc621c6855e522c53f568c82e
Борис Сухобрус <bhobrus@gmail.com>
2022-11-18 15:54:09 +0300

    upd

---
+++ main.py
@@ -0,0 +1,9 @@
+# 5691120266:AAGRU5wI5WImRXmmbJ3u58-a81FUGzscmgs
+# КЛЮЧ ВАШ УНИКАЛЬНЫЙ
+
+import aiogram
+
+API_TOKEN = "5691120266:AAGRU5wI5WImRXmmbJ3u58-a81FUGzscmgs"
+
+bot = aiogram.Bot(token=API_TOKEN)
+dp = aiogram.Dispatcher(bot)
