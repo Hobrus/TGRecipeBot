5c21c985229fd33a46d5506369061d82c8359394
Борис Сухобрус <bhobrus@gmail.com>
2022-11-18 15:55:35 +0300

    upd 2

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ API_TOKEN = "5691120266:AAGRU5wI5WImRXmmbJ3u58-a81FUGzscmgs"
 
 bot = aiogram.Bot(token=API_TOKEN)
 dp = aiogram.Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_message(message: aiogram.types.Message):
+    await message.reply("Hello")
